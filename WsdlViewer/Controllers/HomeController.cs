42dab34ddc143b4dbd324ea7bb9993cf08e542e8
Alex Deane <adeane999@gmail.com>
2026-03-13 10:04:32 -0400

    first attempt at consistent app header but it aint quite right

--- WsdlViewer/Controllers/HomeController.cs
+++ WsdlViewer/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.RateLimiting;
-using WsdlViewer.Models;
 
 namespace WsdlViewer.Controllers;
 
@@ -11,15 +9,6 @@ public class HomeController(WsdlClient.WsdlClient wsdlClient) : Controller
     public IActionResult Index()
     {
         return View();
-    }
-
-    [HttpGet("renderWsdl")]
-    public async Task<IActionResult> RenderWsdl([FromQuery] [Required] string uri)
-    {
-        var xdoc = wsdlClient.GetWsdl(uri);
-        var html = await Transformer.Transformer.Transform(xdoc);
-
-        return Content(html, "text/html");
     }
 
     [HttpPost("renderWsdl")]
@@ -34,16 +23,7 @@ public class HomeController(WsdlClient.WsdlClient wsdlClient) : Controller
         var xdoc = wsdlClient.GetWsdl(effectiveUri);
         var html = await Transformer.Transformer.Transform(xdoc);
 
-        return Content(html, "text/html");
-    }
-
-    [HttpGet("renderXsd")]
-    public async Task<IActionResult> RenderXsd([FromQuery] [Required] string uri)
-    {
-        var xdoc = wsdlClient.GetXsd(uri);
-        var html = await Transformer.Transformer.TransformXsd(xdoc);
-
-        return Content(html, "text/html");
+        return View("Wsdl", html);
     }
 
     [HttpPost("renderXsd")]
@@ -58,15 +38,7 @@ public class HomeController(WsdlClient.WsdlClient wsdlClient) : Controller
         var xdoc = wsdlClient.GetXsd(effectiveUri);
         var html = await Transformer.Transformer.TransformXsd(xdoc);
 
-        return Content(html, "text/html");
-    }
-
-    [HttpGet("TooManyRequests")]
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult TooManyRequests()
-    {
-        ViewBag.ErrorMessage = "You sent too many requests in a short period of time. Please wait a moment and try again.";
-        return View("Error");
+        return View("Xsd", html);
     }
 
     private static async Task<string?> SaveFileOrUseUri(IFormFile? file, string? uri)
